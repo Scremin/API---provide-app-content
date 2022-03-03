8f0e3af19a62c0ca0d74c8deb6a2ed544e56ae2c
Scremin <azflresc@gmail.com>
2022-03-03 15:42:06 +0000

    init db

--- api_a.py
+++ api_a.py
@@ -1,6 +1,8 @@
 # api
 
-from flask import Flask
+from flask import Flask, jsonify
+
+from control_a import *
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -9,7 +11,17 @@ app.url_map.strict_slashes = False
 # -- ROTAS.
 @app.route("/")
 def hominho():
-	return "<h1>Not authorized</h1>"
+
+	try:
+
+		res = get_a()
+
+		return jsonify({'result':res})
+		#return "<h1>Not authorized</h1>"
+
+	except:
+
+		return jsonify({'result':'test failed'})
 
 
 if __name__ == "__main__":
---
+++ control_a.py
@@ -0,0 +1,32 @@
+# control
+
+import pymongo
+from pymongo import MongoClient
+
+
+def get_a():
+
+    try:
+
+        pwd_mongo = process.env.PWD_MONGO #
+        path_db = f"mongodb+srv://mmmmm1:{pwd_mongo}@cluster0.8tcnc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+        
+        # atlas.
+        client = MongoClient(path_db)
+        db = client.app_a
+        collection = db["mmmmm_a"] #
+
+        # -- varrer toda a coleção mmmmm_a.
+        results = collection.find({})
+        li = list()
+        
+        for x in results:
+            li.append(x) # receber todas os elems da coleção mmmmm_a.
+
+        return li
+        #return 'ok'
+
+    except:
+
+        return 'get failed'
+
