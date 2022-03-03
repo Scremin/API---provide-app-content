48431219baab99707849268a10c066e366c015fc
Scremin <azflresc@gmail.com>
2022-03-03 10:58:15 +0000

    init api

---
+++ api_a.py
@@ -0,0 +1,18 @@
+# api
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+# -- ROTAS.
+@app.route("/")
+def hominho():
+
+	return "<h1>Not authorized</h1>"
+
+
+if __name__ == "__main__":
+    app.debug = True
+	app.run(host = '0.0.0.0', port = 5000)
