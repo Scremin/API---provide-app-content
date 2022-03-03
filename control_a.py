99ff6de33db0c1df7c9af19f67518dca252f8c12
Scremin <azflresc@gmail.com>
2022-03-03 15:54:03 +0000

    test con 3

--- control_a.py
+++ control_a.py
@@ -13,19 +13,19 @@ def get_a():
         path_db = f"mongodb+srv://mmmmm1:{pwd_mongo}@cluster0.8tcnc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
         
         # atlas.
-        #client = MongoClient(path_db)
-        #db = client.app_a
-        #collection = db["mmmmm_a"] #
+        client = MongoClient(path_db)
+        db = client.app_a
+        collection = db["mmmmm_a"] #
 
         # -- varrer toda a coleção mmmmm_a.
-        #results = collection.find({})
-        #li = list()
+        results = collection.find({})
+        li = list()
         
-        #for x in results:
-        #    li.append(x) # receber todas os elems da coleção mmmmm_a.
+        for x in results:
+            li.append(x) # receber todas os elems da coleção mmmmm_a.
 
-        #return li
-        return pwd_mongo
+        return li
+        #return pwd_mongo
 
     except:
 
